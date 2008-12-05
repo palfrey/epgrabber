0259df89964cb65d2e4ff1bcd6ac153c4986f841
Tom Parker-Shemilt <palfrey@tevp.net>
2008-12-05 15:58:57 +0000

    Urlgrab is now in github

---
+++ db.py
@@ -0,0 +1,22 @@
+import re
+
+# dattebayo
+def db(inf,prefix):
+	cache = inf["cache"]
+	data = cache.get("http://www.dattebayo.com/t/").read()
+	items = re.findall("/t/([a-z]+)([\d-]+)\.torrent",data)
+	#print items
+	poss = []
+	for (pfx,number) in items:
+		if number.find("-")!=-1:
+			test = int(number.split("-")[0])
+		else:
+			test = int(number)
+		if prefix == pfx and test>inf["season"]:
+			poss.append(number)
+	if len(poss) == 0:
+		return None
+	best = sorted(poss)[0]
+	
+	return {"name":inf["name"],"season":int(best),"epnum":0,"date":inf["yesterday"],"url":"http://www.dattebayo.com/t/%s%s.torrent"%(prefix,best)}
+
---
+++ epgrabber.py
@@ -0,0 +1,631 @@
+#!/usr/bin/python
+
+import sys
+try:
+	from urlgrab.GetURL import GetURL
+	from urlgrab.URLTimeout import URLTimeoutError
+except:
+	print "You need to install urlgrab. Get it using 'darcs get http://tevp.net/projects/urlgrab'"
+	sys.exit(1)
+from re import compile,IGNORECASE,MULTILINE,DOTALL
+from time import strptime,strftime,localtime,time
+from os.path import exists,getsize
+from os import remove
+from urlparse import urljoin
+from datetime import datetime
+from datetime import timedelta,date
+
+from optparse import OptionParser
+
+import urllib
+class AppURLopener(urllib.FancyURLopener):
+    version = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11"
+
+urllib._urlopener = AppURLopener()
+
+
+options = None
+cur = None
+con = None
+cache = None
+
+def saferetrieve(url,fname):
+	try:
+		print "Trying",url
+		#try:
+		#	global cache
+		#	#cache.debug = True
+		#	data = cache.get(url)
+		#except URLTimeoutError,e:
+		#	print e
+		#	return False
+		#file(fname,"wb").write(data.read())
+		urllib.urlretrieve(url,fname)
+		if exists(fname) and getsize(fname)>1000:
+			print "Retrieved!",url
+			return True
+		else:
+			if exists(fname):
+				remove(fname)
+			print "Too small!"
+			return False
+	except IOError:
+		print "IOError!"
+		return False
+
+def scarywater(*args):
+	if len(args)==3:
+		(inf,group,name) = args
+		exclude = ""
+	else:
+		(inf,group,name,exclude) = args
+	global yesterday
+	base = "http://a.scarywater.net/%s/"%group
+	data = cache.get(base,max_age=60*60).read()
+	href = compile("<a href=\"([^\"]+)\"[^>]+>([^<]+)</a>")
+	eps = []
+	added = []
+	crossref = {}
+	for (url,text) in href.findall(data):
+		#print url,text
+		if name not in text or (exclude!="" and exclude in text):
+			continue
+		for x in text.replace("_"," ").replace("-"," ").replace("["," ").split():
+			try:
+				season = int(x)
+				break
+			except ValueError:
+				continue
+		try:
+			if season in added:
+				continue
+		except UnboundLocalError:
+			print url,text
+			raise
+		added.append(season)
+		eps.append((season,0,yesterday,text))
+		crossref[season] = url
+	if eps == []:
+		print "NO SCARYWATER EPISODES"
+		return None
+	ret = core(inf,eps)
+	if ret!=None and crossref.has_key(ret["season"]):
+		ret["url"] = base+crossref[ret["season"]]
+	#print "ret",ret
+	return ret
+
+def geass(inf):
+	ret = wikipedia(inf,"List_of_Code_Geass_episodes")
+	if ret!=None:
+		ret["idnum"] = animeep
+	return ret
+
+trs = compile("<tr[^>]*>(.*?)</tr>",MULTILINE|DOTALL)
+tds = compile("<t(?:d|h)[^>]*>(.*?)</t(?:d|h)>",MULTILINE|DOTALL)
+bold = compile("<b>(.*?)</b>",MULTILINE|DOTALL)
+dm = compile("<a href=\"/wiki/\w+\" title=\"\w+ \w+\">(\w+[- ]\w+)</a>")
+year = compile("<a href=\"/wiki/\d+\" title=\"\d+\">(\d+)</a>")
+title = compile(">([^<]+)</a>")
+
+def wikipedia(inf,page):
+	data = cache.get("http://en.wikipedia.org/wiki/%s"%page,max_age=60*60).read()
+	blocks = trs.findall(data)
+	items = []
+	file("dump","wb").write(data)
+	for b in blocks:
+		if b.find("<td")==-1:
+			continue
+		bits = tds.findall(b)
+		if len(bits)<3:
+			continue
+		blob = {}
+		for bi in bits:
+			if bi.find("List of")!=-1:
+				continue
+			try:
+				try:
+					blob["number"] = str(int(bi))
+				except ValueError:
+					blob["number"] = str(float(bi))
+				continue
+			except ValueError:
+				pass
+			if bi.find("<b>")!=-1: # assume title
+				m = bold.findall(bi)[0]
+				if m[0] in ["\"","\'"]:
+					m = m[1:]
+				if m[-1] in ["\"","\'"]:
+					m = m[:-1]
+				blob["title"] = m
+			elif bi == bits[0]:
+				st = title.findall(bi)
+				if len(st)>0:
+					blob["title"] = st[0]
+			else:
+				try:
+					blob["year"] = year.findall(bi)[0]
+					#print "year",blob["year"]
+				except IndexError:
+					pass
+				try:
+					blob["date"] = dm.findall(bi)[0]
+				except IndexError:
+					pass
+		if len(blob.keys())>0:
+			if not blob.has_key("title"): # guess that first is title
+				print "title?",bits[0]
+			items.append(blob)
+	#raise Exception,items
+	if len(items)==0:
+		raise Exception
+	eps = []
+	#print [ms for ms in items]
+	for ma in items:
+		#print ma
+		if "date" not in ma:
+			continue
+		try:
+			cmb = (ma["date"]+" "+ma["year"]).replace("-"," ")
+		except KeyError:
+			print ma
+			raise
+		try:
+			date = strptime(cmb,"%B %d %Y")
+		except ValueError,e: # alternate format?
+			try:
+				date = strptime(cmb,"%m %d %Y")
+			except ValueError: # no matches
+				print "date doesn't match for",cmb,"discarding"
+				continue
+
+		try:
+			number = ma["number"]
+		except KeyError:
+			print "no valid number. keys are",ma
+			continue
+			
+		if len(number)<3:
+			season = number
+			epnum = 0
+		elif number.find("x")!=-1:
+			(season,epnum) = number.split("x")
+		else:
+			epnum = number[-2:]
+			season = number[:-2]
+		try:
+			eps.append((int(season),int(epnum),date,ma["title"]))
+		except ValueError:
+			print "non-integer season or epnum?",season,epnum
+		except KeyError:
+			print "no valid title. keys are",ma
+	if len(eps) == 0:
+		raise Exception
+	return core(inf,eps)
+	return wikionly(inf,wikipatt,page)
+				
+def epguides(inf,name):
+	data = cache.get("http://epguides.com/%s/"%name,max_age=60*60*24*2).read()
+	patt = compile("\d+.\s+(\d+)-([\s\d]{2})\s*(?:[\dA-Z\-]+)?\s+(\d+ [A-Z][a-z]+ \d+)?\s+<a target=\"(?:visit|_blank)\" href=\"[^\"]+\">([^<]+)</a>")
+	eps = patt.findall(data)
+	if len(eps) ==0:
+		raise Exception
+	neweps = []
+	for e in eps:
+		n = list(e)
+		try:
+			n[2] = strptime(n[2],"%d %b %y")
+		except ValueError:
+			if n[2] != "":
+				print n
+				raise
+			n[2] = None
+		neweps.append(n)
+	return core(inf,neweps)
+
+def multisplit(text,items):
+	out = [text]
+	while len(items)>0:
+		for x in out:
+			sp = x.split(items[0])
+			if len(sp)>1:
+				out.remove(x)
+				out.extend(sp)
+		items = items[1:]
+	print "out",out,text
+	return out
+
+def animeep(title,name,season,epnum):
+	bits = [x.strip() for x in multisplit(title,[" ","."]) if x!="" and x!="-"]
+	print "bits",bits
+	num = -1
+	for x in bits:
+		if x[0] == "[":
+			continue
+		if x.find("-")!=-1:
+			nums = x.split("-")
+			try:
+				while len(nums)>1:
+					val = int(nums[0])
+					if val>season:
+						update(name,int(nums[0]),epnum,force=True)
+						print "updating with",val
+					nums = nums[1:]
+				x = nums[0]
+				if int(nums[0])>season:
+					season = int(nums[0])
+			except ValueError:
+				continue
+		try:
+			num = int(x)
+		except ValueError:
+			continue
+	
+	print "num",num
+	return num == season
+
+def info(name):
+	global cache,yesterday,cur,options
+	#print "options",options
+	keys = ["name","search","season","episode","last","checked"]
+	cur.execute("select %s from series where name=\"%s\""%(",".join(keys),name))
+	ret = {}
+	data = cur.fetchone()
+	for k in range(len(keys)):
+		if hasattr(options,keys[k]) and getattr(options,keys[k])!=-1:
+			ret[keys[k]] = getattr(options,keys[k])
+		else:
+			ret[keys[k]] = data[k]
+	ret["cache"] = cache
+	ret["yesterday"] = yesterday
+	ret["core"] = core
+	ret["options"] = options
+	return ret
+
+def core(inf,eps):
+	if eps == []:
+		raise Exception, "NO EPISODES FOUND"
+	seas = inf["season"]
+	num = inf["episode"]
+	has_prev = False
+
+	eps.sort(lambda a,b:cmp(a[0]*100+a[1],b[0]*100+b[1]))
+		
+	prev = (0,0)
+	last = None
+	for e in eps:
+		(season,epnum,date,title) = e
+		season = int(season)
+		epnum = int(epnum)
+		if (season,epnum) == prev:
+			continue
+		prev = (season,epnum)
+		if season<seas or (season==seas and num>epnum):
+			last = (season,epnum,date)
+			continue
+		print season,epnum,date,has_prev
+		if not has_prev and (season>seas or (season==seas and num!=epnum)):
+			has_prev= True
+		print "%02d-%02d"%(season,epnum),
+		if date !=None:
+			print strftime("%Y-%m-%d",date),
+		else:
+			print "TBA"
+			continue
+		print title
+		if not has_prev:
+			print "has_prev"
+			has_prev = True
+			last = (season,epnum,date)
+			continue
+		else:
+			print "last",last
+		return {"name":inf["name"],"season":season,"epnum":epnum,"date":date}
+	else:
+		print "ran out of episodes!"
+		return None
+
+def	torrent(name,season,epnum):
+	fname = "%s - %02d-%02d.torrent"%(name,season,epnum)
+	print "fname:",fname
+	return fname
+	
+def update(name,season,epnum,force=False):
+	global curr
+	fname = torrent(name,season,epnum)
+	if exists(fname) or force:
+		seas = info(name)["season"]
+		num  = info(name)["episode"]
+		print "season,epnum",season,epnum,seas,num
+		if (season == seas and epnum == num) and not force:
+			print "Duplicate numbers!"
+			return
+		cur.execute("update series set season=%d, episode=%d, last=%d where name=\"%s\""%(season,epnum,curr,name))
+		con.commit()
+		if cur.rowcount!=1:
+			cur.execute("select name,search,season,episode,last from series where name=\"%s\""%ep)
+			print cur.fetchall()
+			raise Exception
+	return fname
+		
+class Isohunt:
+	#row = compile("<tr class=\"hlRow\" onClick=\"servOC\(\d+,\\'/torrent_details/(?P<path>[^']+)'.*?<td class=\"row3\" id=name\d+>(?P<name>.*?)(?=</td>)</td><td class=\"row3\" title='\d+ file(?:s)?'>\d+.\d+ (?:M|G|K)B</td><td class=\"row1\">(?P<seeds>\d*)</td><td class=\"row3\">(?P<peers>\d*)</td>")
+	row = compile("<a onClick=\"servOC\(\d+,\\'/torrent_details/(?P<path>[^']+)'.*?<td class=\"row3\" id=name\d+>.+?tab=summary'>(?P<name>.*?)(?=</td>)</td><td class=\"row3\" title='\d+ file(?:s)?'>\d+.\d+ (?:M|G|K)B</td><td class=\"row1\">(?P<seeds>\d*)</td><td class=\"row3\">(?P<peers>\d*)</td>")
+
+	def rows(self,terms):
+		url ="http://isohunt.com/torrents/%s?ihp=1&iht=-1&ihs1=2&iho1=d"%terms.replace(" ","+")
+		print "url",url
+		torr = cache.get(url,max_age=60*60).read()
+		file('dump','w').write(torr)
+		rows = self.row.finditer(torr)
+		return rows
+
+	def torrent(self,r):
+		return "http://isohunt.com/download/"+r["path"]
+
+class Mininova:
+	row = compile("<a href=\"/get/(?P<path>\d+)\"[^>]*><img src=\"/images/down.gif\" alt=\"\[D\]\"/></a><a href=\"/tor/\d+\">(?P<name>.*?)(?=</a>)</a>.*?(?=</td>)</td><td align=\"right\">\d+.\d+&nbsp;(?:G|M|K)B</td><td align=\"right\"><span class=\"g\">(?P<seeds>\d+)</span></td><td align=\"right\"><span class=\"b\">(?P<peers>\d+)</span>")
+
+	def rows(self,terms):
+		print "terms",terms
+		terms = " ".join([t for t in terms.split(" ") if len(t)>1])
+		print "terms",terms
+		url = "http://www.mininova.org/search/%s/seeds"%terms.replace(" ","+")
+		print "url",url
+		torr = cache.get(url,max_age=60*60).read()
+		rows = self.row.finditer(torr)
+		return rows
+
+	def torrent(self,r):
+		return "http://www.mininova.org/get/%s"%r["path"]
+
+if __name__ == "__main__":
+	idnum = compile("(?:S(\d+)E(\d+))|(?:\[(\d+)x(\d+)\])|(?: (\d)(\d{2}) - )|(?: (\d+)X(\d+) )|(?:\.(\d+)(\d{2}).)|(?: (\d{2}))",IGNORECASE)
+
+	now = list(localtime())
+	for x in range(3,len(now)):
+		now[x] = 0
+	now = tuple(now)
+
+	parser = OptionParser(description="Episode grabber by Tom Parker <palfrey@tevp.net>")
+	parser.add_option("-n","--series",dest="series",action="append",type="string")
+	parser.add_option("-o","--override",dest="override",action="store_true",help="Override normal date values",default=False)
+	parser.add_option("-s","--season",dest="season",type="int",default=-1)
+	parser.add_option("-e","--episode",dest="episode",type="int",default=-1)
+	parser.add_option("-d","--no-download",dest="download",action="store_false",help="Don't download anything",default=True)
+	parser.add_option("-m","--set",dest="save",action="store_true",default=False,help="Store overriden values")
+	parser.add_option("-f","--fast",dest="fast",action="store_true",default=False,help="Find now (ignoring dates)")
+	parser.add_option("--debug",dest="debug",action="store_true",default=False)
+
+	(options,args) = parser.parse_args()
+
+	cache = GetURL(debug=options.debug)
+	cache.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"
+
+	globals()["options"] =  options
+	if len(args)!=0:
+		parser.print_help()
+		parser.error("args after main text")
+
+	try:
+		from pysqlite2 import dbapi2 as sqlite
+	except ImportError,e:
+		print e
+		import sqlite
+
+	con = sqlite.connect("watch.db")
+	cur = con.cursor()
+	globals()["cur"] = cur
+	cur.execute("select name from sqlite_master where type='table' and name='series'")
+	if len(cur.fetchall())==0:
+		cur.execute("create table series (name varchar(30) primary key,search varchar(100),season integer, episode integer, last datetime, command varchar(100), checked datetime);")
+		con.commit()
+	if options.series!=None and len(options.series)>0:
+		cur.execute("select name from series where name='"+("' or name='name='".join(options.series))+"'")
+	else:
+		cur.execute("select name from series order by last desc")
+	d = cur.fetchall()
+	series = [x[0] for x in d]
+	print "d",d
+
+	if len(series)==0:
+		print "You need to add some series!"
+		sys.exit(1)
+
+	print "series",series,"\n"
+
+	#sites = [Isohunt(),Mininova()]
+	sites = [Mininova(),Isohunt()]
+
+	shorttd = timedelta(0,0,0,0,0,6,0)
+	longtd = timedelta(7)
+	limit = timedelta(21)
+
+	curr = time()
+	yesterday = date.fromtimestamp(time())-timedelta(days=1)
+	yesterday = yesterday.timetuple()
+
+	for name in series:
+		print name
+		if options.save:
+			if len(series)>1:
+				raise Exception
+			inf = info(name)
+			if options.season!=-1:
+				inf["season"] = options.season
+			if options.episode!=-1:
+				inf["episode"] = options.episode
+			update(name,inf["season"],inf["episode"],force=True)
+			cur.execute("update series set checked=? where name=?",(curr,name))
+			con.commit()
+			#raise Exception,inf
+
+		cur.execute("select last,checked,command from series where name=?",(name,))
+		((last,checked,command),) = cur.fetchall()
+		if last == None:
+			cur.execute("update series set last=? where name=?",(curr,name))
+			con.commit()
+			last = curr
+		if checked == None:
+			cur.execute("update series set checked=? where name=?",(curr,name))
+			con.commit()
+			checked = curr
+
+		#print "since",curr-last
+		td = timedelta(0,curr-last)
+		
+		if td>limit:
+			mingap = longtd
+		else:
+			mingap = timedelta()#shorttd
+		print "time since last new",td
+		gap = timedelta(0,curr-checked)
+		if not options.override and gap<mingap:
+			print "not enough time has passed. gap is",gap," mingap is",mingap,"\n"
+			continue
+
+		print "command '%s'"%command
+		if command in ["",None]:
+			raise Exception
+		next = None
+		success = False
+		for c in command.split(";"):
+			if c.find("(")!=-1:
+				args = [x.strip() for x in c[c.find("(")+1:c.rfind(")")].split(",")]
+				#print "brk",c[c.find("(")+1:c.find(")")],c.find(")")
+				c = c[:c.find("(")]
+			else:
+				args = []
+			print "cmd: '%s' args: "%c,args
+			cmd = None
+			if c not in globals():
+				try:
+					mod = __import__(c,globals(),locals(),[c])
+					cmd = getattr(mod,c)
+				except ImportError:
+					raise Exception, "can't find command %s"%c
+			if cmd == None:
+				cmd = globals()[c]
+			args = [info(name)]+args
+			try:
+				next = cmd(*args)
+			except URLTimeoutError,e:
+				print "URL TIMEOUT!",e.url
+				continue
+			success = True
+			if next!=None:
+				print "found",next
+				#raise Exception
+				break
+
+		if success or options.save:
+			cur.execute("update series set checked=? where name=?",(curr,name))
+			con.commit()
+		if next!=None:
+			for x in ["season","epnum","date"]:
+				locals()[x] = next[x]
+			delta = None
+			if date!=None:
+				delta = datetime(date[0],date[1],date[2])-datetime(now[0],now[1],now[2])
+				print "delta",delta
+
+			if (date == None or now[:3]<=date[:3]) and not options.fast:
+				print "too early",
+				if date!=None:
+					print now[:3],date[:3],
+					if delta<longtd and td>longtd:
+						cur.execute("update series set last=? where name=?",(curr,name))
+						con.commit()
+				else:
+					print "(don't know next date)",
+				print "\n"
+				continue
+
+			if next.has_key("url"):
+				fname = torrent(name,season,epnum)
+				if options.download:
+					if not saferetrieve(next["url"],fname):
+						continue
+				update(name,season,epnum)
+				con.commit()
+				print ""
+				continue
+			gotit = False
+			for site in sites:
+				try:
+					patt = ""
+					if season!=0:
+						patt += " %d"%season
+					if epnum!=0:
+						patt +=" %d"%epnum
+					rows = site.rows(info(name)["search"]+ " -zip -rar -ita -crimson -raw -mkv -720"+patt)
+					print site
+					newrows = []
+					for nr in rows:
+						r = nr.groupdict()
+						#print "r",r
+						try:
+							r["seeds"] = int(r["seeds"])
+						except ValueError:
+							r["seeds"] = 0
+						try:
+							r["peers"] = int(r["peers"])
+						except ValueError:
+							r["peers"] = 0
+						newrows.append(r)
+
+					rows = newrows
+					#assert(rows!=[])
+					rows.sort(lambda x,y:cmp(y["seeds"],x["seeds"]))
+						
+					for r in rows:
+						sp = "<span title=\""
+						if r["name"].find(sp)!=-1:
+							r["name"] = r["name"][r["name"].find(sp)+len(sp):r["name"][len(sp)+1:].find("\"")+len(sp)+1]
+						r["name"] = r["name"].replace("<b>","").replace("</b>","")
+						ok = False
+						print "row",r["name"]
+						if next.has_key("idnum"):
+							print "options",options
+							globals()["options"] = options
+							try:
+								ok = next["idnum"](r["name"],name,season,epnum)
+							except TypeError:
+								ok = globals()[next["idnum"]](r["name"],name,season,epnum)
+						else:
+							num = idnum.search(r["name"])
+							if num!=None:
+								print num.groups()
+								try:
+									which = [int(x) for x in num.groups() if x!=None]
+								except TypeError:
+									print r["name"],num.groups()
+									raise
+								if len(which) == 1:
+									which += [0]
+								if which == [season,epnum]:
+									ok = True
+								else:
+									print "wrong ep, want",(season,epnum),"got",which
+
+								print r,which
+						if ok:
+							fname = torrent(name,season,epnum)
+							if options.download:
+								if not saferetrieve(site.torrent(r),fname):
+									continue
+							update(name,season,epnum)
+							con.commit()
+							gotit = True
+							break
+						else:
+							print "not an ep",r
+							print
+							
+				except URLTimeoutError:
+					print "URLTimeout for",site
+					continue
+				if gotit:
+					break
+			else:
+				print "can't get %d-%d for %s"%(season,epnum,name)
+		print ""
