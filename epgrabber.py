d58ae42497a7be769b4965683ea354548c9ec2b5
Tom Parker-Shemilt <palfrey@tevp.net>
2008-12-05 16:10:27 +0000

    Split out the scarywater code

--- epgrabber.py
+++ epgrabber.py
@@ -53,47 +53,6 @@ def saferetrieve(url,fname):
 		print "IOError!"
 		return False
 
-def scarywater(*args):
-	if len(args)==3:
-		(inf,group,name) = args
-		exclude = ""
-	else:
-		(inf,group,name,exclude) = args
-	global yesterday
-	base = "http://a.scarywater.net/%s/"%group
-	data = cache.get(base,max_age=60*60).read()
-	href = compile("<a href=\"([^\"]+)\"[^>]+>([^<]+)</a>")
-	eps = []
-	added = []
-	crossref = {}
-	for (url,text) in href.findall(data):
-		#print url,text
-		if name not in text or (exclude!="" and exclude in text):
-			continue
-		for x in text.replace("_"," ").replace("-"," ").replace("["," ").split():
-			try:
-				season = int(x)
-				break
-			except ValueError:
-				continue
-		try:
-			if season in added:
-				continue
-		except UnboundLocalError:
-			print url,text
-			raise
-		added.append(season)
-		eps.append((season,0,yesterday,text))
-		crossref[season] = url
-	if eps == []:
-		print "NO SCARYWATER EPISODES"
-		return None
-	ret = core(inf,eps)
-	if ret!=None and crossref.has_key(ret["season"]):
-		ret["url"] = base+crossref[ret["season"]]
-	#print "ret",ret
-	return ret
-
 def geass(inf):
 	ret = wikipedia(inf,"List_of_Code_Geass_episodes")
 	if ret!=None:
