e1ce62df7f44affe6e1604259750a98d588626d2
Tom Parker-Shemilt <palfrey@tevp.net>
2009-02-03 01:56:23 +0000

    Add initial version of gui interface

---
+++ gui.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys
+import pygtk
+pygtk.require("2.0")
+import gtk
+import gobject
+from pysqlite2 import dbapi2 as sqlite
+from time import *
+
+class EpgrabberGUI:
+	def __init__(self):
+		
+		#Set the Glade file
+		self.intffile = "epgrabber.glade"  
+		self.wTree = gtk.Builder()
+		self.wTree.add_from_file(self.intffile)
+		
+		#Get the Main Window, and connect the "destroy" event
+		self.window = self.wTree.get_object("wndMain")
+		if self.window:
+			self.window.connect("destroy", gtk.main_quit)
+		self.window.show()
+
+		self.episodes = gtk.ListStore(gobject.TYPE_STRING,gobject.TYPE_STRING,gobject.TYPE_UINT, gobject.TYPE_UINT,gobject.TYPE_STRING, gobject.TYPE_STRING) # name, search, series, episode, command
+		con = sqlite.connect("watch.db")
+		cur = con.cursor()
+		cur.execute("select name,search,season,episode,command,last from series order by last desc")
+		for row in cur.fetchall():
+			self.episodes.append(row)
+		
+		def build_tree_column(name,column):
+			editable = gtk.CellRendererText()
+			editable.set_property('editable', True)
+			editable.connect('edited', self.edit_data,(self.episodes,column))
+			return gtk.TreeViewColumn(name,editable,text=column)
+
+		self.episodesList = self.wTree.get_object("tblEpisodes")
+		self.episodesList.set_model(self.episodes)
+		self.episodesList.append_column(build_tree_column("Name",0))
+		self.episodesList.append_column(gtk.TreeViewColumn("Search",gtk.CellRendererText(),text=1))
+		self.episodesList.append_column(gtk.TreeViewColumn("Series",gtk.CellRendererText(),text=2))
+		self.episodesList.append_column(gtk.TreeViewColumn("Episode",gtk.CellRendererText(),text=3))
+		self.episodesList.append_column(gtk.TreeViewColumn("Command",gtk.CellRendererText(),text=4))
+		
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn("Last retrieved",cell,text=5)
+		col.set_cell_data_func(cell,self.date_field,5)
+		self.episodesList.append_column(col)
+
+	def date_field(self, column, cell, model, iter, user_data):
+		when = float(model.get_value(iter, user_data))
+		if when == 0:
+			when = "<span foreground=\"red\">Never</span>"
+		else:
+			when = strftime("%a, %d %b %Y",localtime(when))
+		cell.set_property('markup', when)
+	
+	def edit_data(self):
+		pass
+
+
+
+
+if __name__ == "__main__":
+	main = EpgrabberGUI()
+	gtk.main()
