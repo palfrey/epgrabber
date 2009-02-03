e9905d6e0947c44cee07d5d1db8fa510b2fb68e4
Tom Parker-Shemilt <palfrey@tevp.net>
2009-02-03 17:09:21 +0000

    Check for gtk >=2.12 for gtk.Builder

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-import pygtk
-pygtk.require("2.0")
 import gtk
+
+ver = gtk.check_version(2,12,0)
+if ver:
+	raise Exception,ver
+
 import gobject
 from pysqlite2 import dbapi2 as sqlite
 from time import *
