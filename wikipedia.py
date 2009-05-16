a5f211310c1be1096a9962e8782c7d687b20a2c2
Tom Parker-Shemilt <palfrey@tevp.net>
2009-05-16 22:53:55 +0100

    Make wikipedia module viable again, and improve date parsing

--- wikipedia.py
+++ wikipedia.py
@@ -1,11 +1,16 @@
+from re import compile,IGNORECASE,MULTILINE,DOTALL
+from time import strptime, struct_time
+
 trs = compile("<tr[^>]*>(.*?)</tr>",MULTILINE|DOTALL)
 tds = compile("<t(?:d|h)[^>]*>(.*?)</t(?:d|h)>",MULTILINE|DOTALL)
 bold = compile("<b>(.*?)</b>",MULTILINE|DOTALL)
 dm = compile("<a href=\"/wiki/\w+\" title=\"\w+ \w+\">(\w+[- ]\w+)</a>")
 year = compile("<a href=\"/wiki/\d+\" title=\"\d+\">(\d+)</a>")
 title = compile(">([^<]+)</a>")
+tags = compile(r'<[^<]*?/?>')
 
 def wikipedia(inf,page):
+	cache = inf["cache"]
 	data = cache.get("http://en.wikipedia.org/wiki/%s"%page,max_age=60*60).read()
 	blocks = trs.findall(data)
 	items = []
@@ -17,6 +22,7 @@ def wikipedia(inf,page):
 		if len(bits)<3:
 			continue
 		blob = {}
+		#print "bits",bits
 		for bi in bits:
 			if bi.find("List of")!=-1:
 				continue
@@ -28,26 +34,37 @@ def wikipedia(inf,page):
 				continue
 			except ValueError:
 				pass
-			if bi.find("<b>")!=-1: # assume title
-				m = bold.findall(bi)[0]
-				if m[0] in ["\"","\'"]:
-					m = m[1:]
-				if m[-1] in ["\"","\'"]:
-					m = m[:-1]
-				blob["title"] = m
-			elif bi == bits[0]:
-				st = title.findall(bi)
-				if len(st)>0:
-					blob["title"] = st[0]
-			else:
-				try:
-					blob["year"] = year.findall(bi)[0]
-					#print "year",blob["year"]
-				except IndexError:
-					pass
-				try:
+			if "title" not in blob:
+				if bi.find("<b>")!=-1: # assume title
+					m = bold.findall(bi)[0]
+					if m.find("<")!=-1:
+						m = m[:m.find("<")]
+					if m[0] in ["\"","\'"]:
+						m = m[1:]
+					if m[-1] in ["\"","\'"]:
+						m = m[:-1]
+					blob["title"] = m
+					continue
+				elif bi == bits[0]:
+					st = title.findall(bi)
+					if len(st)>0:
+						blob["title"] = st[0]
+						continue
+			try:
+				blob["year"] = year.findall(bi)[0]
+				#print "year",blob["year"]
+			except IndexError:
+				pass
+			try:
+				if bi.find("200")!=-1: # first bit of 200?
 					blob["date"] = dm.findall(bi)[0]
-				except IndexError:
+			except IndexError:
+				try:
+					if bi.find("<")!=-1:
+						bi = bi[:bi.find("<")]
+					blob["date"] = strptime(bi,"%B %d, %Y")
+				except ValueError:
+					print "not date",bi
 					pass
 		if len(blob.keys())>0:
 			if not blob.has_key("title"): # guess that first is title
@@ -59,22 +76,25 @@ def wikipedia(inf,page):
 	eps = []
 	#print [ms for ms in items]
 	for ma in items:
-		#print ma
+		#print "ma",ma
 		if "date" not in ma:
 			continue
-		try:
-			cmb = (ma["date"]+" "+ma["year"]).replace("-"," ")
-		except KeyError:
-			print ma
-			raise
-		try:
-			date = strptime(cmb,"%B %d %Y")
-		except ValueError,e: # alternate format?
+		if type(ma["date"]) == struct_time:
+			date = ma["date"]
+		else:
 			try:
-				date = strptime(cmb,"%m %d %Y")
-			except ValueError: # no matches
-				print "date doesn't match for",cmb,"discarding"
-				continue
+				cmb = (ma["date"]+" "+ma["year"]).replace("-"," ")
+			except KeyError:
+				print ma
+				raise
+			try:
+				date = strptime(cmb,"%B %d %Y")
+			except ValueError,e: # alternate format?
+				try:
+					date = strptime(cmb,"%m %d %Y")
+				except ValueError: # no matches
+					print "date doesn't match for",cmb,"discarding"
+					continue
 
 		try:
 			number = ma["number"]
@@ -83,8 +103,8 @@ def wikipedia(inf,page):
 			continue
 			
 		if len(number)<3:
-			season = number
-			epnum = 0
+			season = 1
+			epnum = number
 		elif number.find("x")!=-1:
 			(season,epnum) = number.split("x")
 		else:
@@ -98,4 +118,4 @@ def wikipedia(inf,page):
 			print "no valid title. keys are",ma
 	if len(eps) == 0:
 		raise Exception
-	return core(inf,eps)
+	return inf["core"](inf,eps)
