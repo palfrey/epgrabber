69a31ac06848d10a6d59272df63a19e20f6855f5
Tom Parker-Shemilt <palfrey@tevp.net>
2010-01-06 14:50:56 +0000

    Move enum to fetch as that's the only place it's needed

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -2,7 +2,7 @@ from os import listdir
 from os.path import dirname
 keep = []
 for f in listdir(dirname(__file__)):
-	if f[-3:] == ".py" and f[0] != "_":
+	if f[-3:] == ".py" and f[0] != "_" and f!="enum.py":
 		s = f[:-3]
 		locals()[s] = getattr(__import__("fetch.%s"%s,globals(),locals(),[s]),s)
 		keep.append(s)
