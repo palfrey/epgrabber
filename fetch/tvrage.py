b1891518766950fc63e5cf082ab37e109f2d9dca
Tom Parker-Shemilt <palfrey@tevp.net>
2013-08-22 20:10:43 +0100

    Start using the TVRage services, which have much better data!

--- fetch/tvrage.py
+++ fetch/tvrage.py
@@ -1,42 +1,23 @@
-# http://www.tvrage.com/DoctorWho_2005/episode_guide/all
-
 import re
 from time import strptime
+from xml.dom import minidom
 
 class tvrage:
-	args = {"name":"Tvrage page name e.g. http://www.tvrage.com/NAME"}
+	args = {"sid":"Tvrage id from http://services.tvrage.com/info.php?page=main"}
 
-	epPattern = re.compile("<h1 class=\"content_title hover_blue\">(.*?)<br />")
-	titlePattern = re.compile("<a [^>]+>(.*?)</a>")
-	datePattern = re.compile("<i>(.*?)</i>")
-
-	def run(self,inf,name):
-		url = "http://www.tvrage.com/%s/episode_guide/all/"%name
+	def run(self,inf,sid):
+		url = "http://services.tvrage.com/feeds/episode_list.php?sid=%s"%sid
 		data = inf["cache"].get(url,max_age=60*60*24*2).read()
 		open("dump","wb").write(data)
-		eps = tvrage.epPattern.findall(data)
+		xml = minidom.parseString(data)
 		neweps = []
-		for ep in eps:
-			title = tvrage.titlePattern.search(ep)
-			title = title.groups()[0]
-			date = tvrage.datePattern.search(ep)
-			if date == None:
-				print "no date for", title
-				continue
-			date = date.groups()[0]
-			if date.find("/")==-1:
-				print "no full date for", title, date
-				continue
-			date = strptime(date,"%b/%d/%Y")
-			title = title[title.find(":")+1:]
-			if title.find("-") == -1 or title.find("x") ==-1:
-				print "not a proper episode", title
-				continue
-			epdata, title = title.split("-", 2)
-			title = title.strip()
-			season, epnum = epdata.split("x")
-			print title, date, season, epnum
-			neweps.append((int(season), int(epnum), date,title))
-		#print eps
+		for seasonNode in xml.getElementsByTagName("Season"):
+			season = seasonNode.getAttribute("no")
+			for ep in seasonNode.getElementsByTagName("episode"):
+				title = ep.getElementsByTagName("title")[0].firstChild.data
+				date = strptime(ep.getElementsByTagName("airdate")[0].firstChild.data,"%Y-%m-%d")
+				epnum = ep.getElementsByTagName("seasonnum")[0].firstChild.data
+				#print title, date, season, epnum
+				neweps.append((int(season), int(epnum), date,title))
 		return inf["core"](inf,neweps)
 
