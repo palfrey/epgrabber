5154373aad74cb05709be4690d04222cab1a9e6c
Tom Parker-Shemilt <palfrey@tevp.net>
2016-11-13 21:10:38 +0000

    Redo to new Torrentz site

--- epgrabber.py
+++ epgrabber.py
@@ -376,11 +376,13 @@ class KAT:
 
 class Torrentz:
 	#<dl><dt><a href="/f0e1c5ba695ec3071ce2390a7466138adf9a4455"><b>Arrow</b> S02E04 HDTV x264 LOL ettv</a> &#187; sdtv tv divx xvid video shows</dt><dd><span class="v" style="color:#fff;background-color:#79CC53">5&#10003;</span><span class="a"><span title="Thu, 31 Oct 2013 01:04:29">10 days</span></span><span class="s">279 MB</span> <span class="u">7,855</span><span class="d">530</span></dd></dl>
-	row = compile("<dl><dt><a href=\"(?P<path>/[a-z0-9]+)\">(?P<name>.*?)</a>.*?</dt><dd>.*?<span class=\"u\">(?P<seeds>[\d,]+)</span><span class=\"d\">(?P<peers>[\d,]+)</span>", MULTILINE|DOTALL|UNICODE)
+	#row = compile("<dl><dt><a href=\"?(?P<path>/[a-z0-9]+)\"?>(?P<name>.*?)</a>.*?</dt><dd>.*?<span class=\"u\">(?P<seeds>[\d,]+)</span><span class=\"d\">(?P<peers>[\d,]+)</span>", MULTILINE|DOTALL|UNICODE)
+	#row = compile("<dl><dt><a href=(?P<path>/[a-z0-9]+)>(?P<name>.*?)</a>.*?</dt><dd><span>✓</span><span title=\d+>([^<]+)</span><span>(\d+ (?:M|G)B)</span><span>(?P<peers>\d+)</span>", MULTILINE|DOTALL|UNICODE)
+	row = compile("<dl><dt><a href=(?P<path>/[a-z0-9]+)>(?P<name>.*?)</a>", MULTILINE|DOTALL|UNICODE)
 	#row = compile("class=\"epinfo\">(?P<name>[^<]+)</a>\s+</td>\s+<td align=\"center\" class=\"forum_thread_post\">(?P<allpath>(?:<a href=\"(?P<path>[^\"]+)\" class=\"[^\"]+\" title=\"[^\"]+\"></a>)+)",MULTILINE|DOTALL|UNICODE)
 
 	def rows(self,terms, numbers):
-		url = "https://torrentz.eu/search?f=%s" % terms 
+		url = "https://torrentz2.eu/search?f=%s" % terms
 		print url
 		torr = cache.get(url, max_age=60*60).read()
 
@@ -392,7 +394,7 @@ class Torrentz:
 	        return checkterms(terms, rows)
 	
 	def torrent(self,r):
-		url = "https://torrentz.eu" + r['path']
+		url = "https://torrentz2.eu" + r['path']
 		page = cache.get(url, max_age = -1).read()
 		links = findall("<a href=\"([^\"]+?)\" rel=\"e\">", page)
 
@@ -518,7 +520,7 @@ def run(options, parser):
 	
 	print "Selected series:",(", ".join(sorted(series))),"\n"
 
-	main_sites = [KAT(), Torrentz(), EZTV()]
+	main_sites = [EZTV()]
 
 	shorttd = timedelta(0,0,0,0,0,6,0)
 	longtd = timedelta(7)
