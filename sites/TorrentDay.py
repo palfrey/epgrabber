86563f616089c7b8a30307ed17fbccd3a8f9181f
Tom Parker-Shemilt <palfrey@tevp.net>
2016-12-06 20:22:40 +0000

    Upgrade TorrentDay to "not rars"

--- sites/TorrentDay.py
+++ sites/TorrentDay.py
@@ -1,9 +1,9 @@
 from codecs import open
-from re import compile
+from re import compile, MULTILINE, DOTALL
 import os.path
 
 class TorrentDay:
-	row = compile("<tr><td class=t_label><a href=\"\?\d+\"><img src=\"/images/categories/[^\"]+\"></a></td><td class=torrentNameInfo><a class=\"\" href=\"(?P<path>/details.php\?id=\d+)\">(?P<name>[^<]+)</a>.*?<td class=ac><a href=\"(?P<download>/download.php/\d+/[^\"]+)\">.*?<td class=\"ac seedersInfo\">(?P<seeds>\d+)</td><td class=\"ac leechersInfo\">(?P<peers>\d+)</td></tr>")
+	row = compile("<td class=\"torrentNameInfo\"><div class='torrentTableNameDiv'><a class='torrentName' href='details.php\?id=\d+'><b>(?P<name>[^<]+)</b></a>.*?<a class=\"index\" href=\"(?P<download>download.php/\d+/[^\"]+)\"><img src=\"/images/browse/uTorrent.png\" alt=\"Download as .torrent file\" border=0></a></td><td class=\"browse\"  align=\"center\">0</td>.*?<td class=\"ac seedersInfo\">(?P<seeds>\d+)</td>.*?<td class=\"ac leechersInfo\">(?P<peers>\d+)</td>", MULTILINE|DOTALL)
 
 	cookie = open(os.path.join(os.path.dirname(__file__), "td-cookie")).read()
 
@@ -14,7 +14,7 @@ class TorrentDay:
 		# TorrentDay doesn't like exclusion terms :(
 		s = terms.split(" ")
 		terms = "-".join([x for x in s if x[0] != "-"])
-		url = "https://torrentday.it/t?32=&33=&14=&26=&7=&34=&2=&q=%s" % (terms.replace(" ", "%20"))
+		url = "https://torrentday.it/browse.php?search=%s&cata=yes&c32=1&c31=1&c33=1&c7=1&c34=1&c2=1" % (terms.replace(" ", "%20"))
 		print url
 		torr = self.cache.get(url, max_age=60*60, headers={"Cookie": self.cookie}).read()
 		rows = list(self.row.finditer(torr))
@@ -25,4 +25,4 @@ class TorrentDay:
 		return rows
 
 	def torrent(self, row):
-		return [{"url":"https://torrentday.it" + row["download"], "headers":{"Cookie":self.cookie}}]
+		return [{"url":"https://torrentday.it/" + row["download"], "headers":{"Cookie":self.cookie}}]
