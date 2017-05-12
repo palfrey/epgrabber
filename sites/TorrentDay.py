740ac516fb32d0fcaaad3c5390cdf44a258b8652
Tom Parker-Shemilt <palfrey@tevp.net>
2017-05-12 19:19:23 +0100

    Make TorrentDay bug out if it hasn't gotten enough rows

--- sites/TorrentDay.py
+++ sites/TorrentDay.py
@@ -3,7 +3,8 @@ from re import compile, MULTILINE, DOTALL
 import os.path
 
 class TorrentDay:
-	row = compile("<td class=\"torrentNameInfo\"><div class='torrentTableNameDiv'><a class='torrentName' href='details.php\?id=\d+'><b>(?P<name>[^<]+)</b></a>.*?<a class=\"index\" href=\"(?P<download>download.php/\d+/[^\"]+)\"><img src=\"/images/browse/uTorrent.png\" alt=\"Download as .torrent file\" border=0></a></td><td class=\"browse\"  align=\"center\">0</td>.*?<td class=\"ac seedersInfo\">(?P<seeds>\d+)</td>.*?<td class=\"ac leechersInfo\">(?P<peers>\d+)</td>", MULTILINE|DOTALL)
+	row = compile("<td class=\"torrentNameInfo\"><div class='torrentTableNameDiv'><a class='torrentName' href='details.php\?id=\d+'><b>(?P<name>[^<]+)</b></a>.*?<a class=\"index\" href=\"(?P<download>download.php/\d+/[^\"]+)\"><img src=\"/images/browse/uTorrent.png\" alt=\"Download as .torrent file\" border=0></a></td>.*?<td class=\"ac seedersInfo\">(?P<seeds>[\d,]+)</td>.*?<td class=\"ac leechersInfo\">(?P<peers>[\d,]+)</td>", MULTILINE|DOTALL)
+	rowcount = compile("<div align='center' class='paging'><span>1-(\d+)</span>")
 
 	cookie = open(os.path.join(os.path.dirname(__file__), "td-cookie")).read()
 
@@ -13,11 +14,16 @@ class TorrentDay:
 	def rows(self, terms, numbers):
 		# TorrentDay doesn't like exclusion terms :(
 		s = terms.split(" ")
-		terms = "-".join([x for x in s if x[0] != "-"])
-		url = "https://torrentday.it/browse.php?search=%s&cata=yes&c32=1&c31=1&c33=1&c7=1&c34=1&c2=1" % (terms.replace(" ", "%20"))
+		terms = "+".join([x for x in s if x[0] != "-"])
+		url = "https://torrentday.it/browse.php?search=%s&cata=yes&sort=7&type=desc" % (terms.replace(" ", "+"))
 		print url
 		torr = self.cache.get(url, max_age=60*60, headers={"Cookie": self.cookie}).read()
 		rows = list(self.row.finditer(torr))
+		expected = int(self.rowcount.findall(torr)[0])
+		if len(rows) != expected:
+		    for row in rows:
+			print row.groups()
+		    raise Exception, (len(rows), expected)
 		if rows == []:
 			print terms
 			open("dump",mode = "wb", encoding="utf-8").write(torr)
