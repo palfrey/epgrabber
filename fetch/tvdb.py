df00489af64cadbc9271cdf1405342deb1d3d0da
Tom Parker-Shemilt <palfrey@tevp.net>
2018-05-08 20:59:28 +0100

    Add support for TVDB api

--- fetch/tvdb.py
+++ fetch/tvdb.py
@@ -1,19 +1,38 @@
 from re import compile
 from time import strptime
 from codecs import open
+import json
+import requests
+import os.path
 
 class tvdb:
 	args = {"sid":"Tvdb id"}
 
-	row = compile("<tr><td class=\"(?:odd|even)\"><a href=\"/\?tab=episode&seriesid=\d+&seasonid=\d+&id=\d+&amp;lid=\d+\">(?P<season>\d+) x (?P<episode>\d+)</a></td><td class=\"(?:odd|even)\"><a href=\"/\?tab=episode&seriesid=\d+&seasonid=\d+&id=\d+&amp;lid=\d+\">(?P<name>[^<]*)</a></td><td class=\"(?:odd|even)\">(?P<date>\d+-\d+-\d+)</td>")
+	login = json.loads(open(os.path.join(os.path.dirname(__file__), "tvdb-login")).read())
+
+	token = None
+
+	def get_token(self):
+		print "getting token"
+		t = requests.post("https://api.thetvdb.com/login", json=self.login)
+		t.raise_for_status()
+		self.token = t.json()["token"]
 
 	def run(self,inf,sid):
-		url = "http://thetvdb.com/?tab=seasonall&id=%s"%sid
-		data = inf["cache"].get(url,max_age=60*60*12).read()
-		#open("dump","wb", encoding="utf-8").write(data)
-		rows = tvdb.row.findall(data)
-		#print rows
+		url = "https://api.thetvdb.com/series/%s/episodes" % sid
+		if not inf["cache"].has_item(url):
+			if self.token == None:
+				self.get_token()
+
+		data = inf["cache"].get(url, headers={"Authorization": "Bearer %s" % self.token}, max_age=60*60*24*2).read()
+		episodes = json.loads(data)["data"]
+		if len(episodes) > 100:
+			raise Exception
 		neweps = []
-		for (season, epnum, title, date) in rows:
-			neweps.append((int(season), int(epnum), strptime(date,"%Y-%m-%d"), title))
+		for ep in episodes:
+			try:
+				neweps.append((ep["airedSeason"], ep["airedEpisodeNumber"], strptime(ep["firstAired"], "%Y-%m-%d") if ep["firstAired"] != "" else None, ep["episodeName"]))
+			except:
+				print ep
+				raise
 		return inf["core"](inf,neweps)
